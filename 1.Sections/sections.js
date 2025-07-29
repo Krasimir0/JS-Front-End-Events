bb1e2a85d9e790f31bc8771097d62783ef674eb0
Krasimir Petranov <83006302+Krasimir0@users.noreply.github.com>
2025-07-29 18:18:12 +0100

    Finish exercise 1

--- 1.Sections/sections.js
+++ 1.Sections/sections.js
@@ -1,5 +1,27 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-   // TODO
+   const sections = document.getElementById("task-input")[0];
+   const submitBtn = document.getElementById("task-input")[1];
+   const divContent = document.getElementById("content");
+   
+   submitBtn.addEventListener('click', (e) => {
+      e.preventDefault();
+      let arrOfSections = sections.value.split(', ');
+      for (const section of arrOfSections) {
+         let div = document.createElement('div');
+         let p = document.createElement('p');
+         p.textContent = section;
+         p.style.visibility = "hidden";
+
+         p.addEventListener('click', () => {
+            p.style.visibility = "visible";
+          }); 
+         
+         div.appendChild(p);
+         divContent.appendChild(div);
+      }
+   });
+
+
 }
