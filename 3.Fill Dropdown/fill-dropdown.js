dabf5a9d94c2fb6dd2a86e13f1e868c87d745b32
Krasimir Petranov <83006302+Krasimir0@users.noreply.github.com>
2025-07-30 15:08:11 +0100

    Finish exercise 3

--- 3.Fill Dropdown/fill-dropdown.js
+++ 3.Fill Dropdown/fill-dropdown.js
@@ -1,5 +1,21 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-
+     const form = document.querySelector("form");
+     const menu = document.getElementById("menu");
+      
+      form.addEventListener("submit", (e) => {
+         e.preventDefault();
+         const newItemText = document.getElementById("newItemText").value;
+         const newItemValue = document.getElementById("newItemValue").value;
+    
+         const option = document.createElement("option");
+         option.textContent = newItemText;
+         option.value = newItemValue;
+         
+         menu.appendChild(option);
+        
+         newItemText = '';
+         newItemValue = '';
+      });
 }
