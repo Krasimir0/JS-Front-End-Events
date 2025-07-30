32e28c313cebc89b098214126fdab3bcc2d52dc4
Krasimir Petranov <83006302+Krasimir0@users.noreply.github.com>
2025-07-30 14:24:00 +0100

    Finish exercise 2

--- 2.Locked Profiles/locked-profile.js
+++ 2.Locked Profiles/locked-profile.js
@@ -1,5 +1,21 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-    //TODO
+    const showMoreBtns = document.getElementsByTagName("button");
+    
+    for (const btn of showMoreBtns) {
+        btn.addEventListener("click", e => {
+            e.preventDefault();
+            const profile = e.target.closest(".profile");
+            const unlockInput = profile.querySelector('input[id$="Unlock"]');
+            
+            if (unlockInput.checked) {
+                const hiddenFields = profile.querySelector('.hidden-fields');
+                const isVisible = hiddenFields.style.display === 'block';
+
+                hiddenFields.style.display = isVisible ? 'none' : 'block';
+                e.target.textContent = isVisible ? 'Show more' : 'Hide it';
+            }
+        })   
+    }
 }
